f51e2607e35f1fc973ef99dcf305306aea94002a
GrahamGunlock <gegunlock@gmail.com>
2016-05-21 21:15:27 -0500

    Update ps3cont.py

--- ps3cont.py
+++ ps3cont.py
@@ -8,7 +8,10 @@ gamepad = ds3.Controller()
 if not gamepad.connect():
     print "Could not connect to PS3 controller"
     sys.exit(1)
-    
+
+
+running = True
+speed = 0
 NOISEUP = 153.5
 NOISEDWN = 102.5
 motorA = ev3.LargeMotor('outA')
@@ -24,7 +27,6 @@ for event in gamepad.read_loop():
     if event.stick_moved(ds3.LT_STICK, ds3.Y_AXIS):
         if rawinput < NOISEDWN or rawinput > NOISEUP:
                 motorB.run_forever(duty_cycle_sp=event.get_speed())
-                print "Left"
         else:
             motorB.run_forever(duty_cycle_sp=0)
     elif event.btn_down(ds3.BTN_X):
