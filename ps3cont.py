ff63975069d50399ade2afcc2b6c69c81c3233d2
GrahamGunlock <gegunlock@gmail.com>
2016-05-21 21:49:35 -0500

    Update ps3cont.py

--- ps3cont.py
+++ ps3cont.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-
-import sys
+import threading
 import dualshock3 as ds3
 import ev3dev.ev3 as ev3
+
 
 gamepad = ds3.Controller()
 if not gamepad.connect():
@@ -11,24 +11,51 @@ if not gamepad.connect():
 
 
 running = True
-speed = 0
+speedA = 0
+speedB = 0
 NOISEUP = 153.5
 NOISEDWN = 102.5
-motorA = ev3.LargeMotor('outA')
-motorB = ev3.LargeMotor('outB')
+
+
+class MotorThread(threading.Thread):
+    motorA = ev3.LargeMotor('outA')
+    motorB = ev3.LargeMotor('outB')
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        print "Engine running!"
+        lastspeedA=0
+        lastspeedB=0
+        while running:
+            if lastspeedA != speedA:
+                self.motorA.run_forever(duty_cycle_sp=speedA)
+                lastspeedA = speedA
+            if lastspeedB != speedB:
+                self.motorB.run_forever(duty_cycle_sp=speedB)
+                lastspeedB = speedB
+
+        self.motorA.stop()
+        self.motorB.stop()
+
+
+motor_thread = MotorThread()
+motor_thread.setDaemon(True)
+motor_thread.start()
+
 
 for event in gamepad.read_loop():
-    rawinput = event.raw_event.value
     if event.stick_moved(ds3.RT_STICK, ds3.Y_AXIS):
         if rawinput < NOISEDWN or rawinput > NOISEUP:
-            motorA.run_forever(duty_cycle_sp=event.get_speed())
+            speedA = event.get_speed()
         else:
-            motorA.run_forever(duty_cycle_sp=0)
-    if event.stick_moved(ds3.LT_STICK, ds3.Y_AXIS):
+            speedA = 0
+    elif event.stick_moved(ds3.LT_STICK, ds3.Y_AXIS):
         if rawinput < NOISEDWN or rawinput > NOISEUP:
-                motorB.run_forever(duty_cycle_sp=event.get_speed())
+            speedB = event.get_speed()
         else:
-            motorB.run_forever(duty_cycle_sp=0)
+            speedB = 0
     elif event.btn_down(ds3.BTN_X):
         print "SHUTTING DOWN"
-        sys.exit(1)
+        running = False
+        break
